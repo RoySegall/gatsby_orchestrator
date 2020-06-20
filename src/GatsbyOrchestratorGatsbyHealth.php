7acd5c67dc7e16eb183dd6e4cb65f9acea835ffa
RoySegall <roy@segall.io>
2020-06-20 12:57:13 +0300

    Adding the first test

--- src/GatsbyOrchestratorGatsbyHealth.php
+++ src/GatsbyOrchestratorGatsbyHealth.php
@@ -2,13 +2,39 @@
 
 namespace Drupal\gatsby_orchestrator;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Messenger\Messenger;
+use Drupal\Core\Messenger\MessengerInterface;
+use GuzzleHttp\ClientInterface;
 use Symfony\Component\HttpFoundation\Response;
 
 class GatsbyOrchestratorGatsbyHealth {
 
+  /**
+   * The status when the gatsby development server is not running.
+   */
   const GATSBY_SERVICE_DOWN = 0;
+
+  /**
+   * The status when the gatsby development server is running.
+   */
   const GATSBY_SERVICE_UP = 1;
+
+  /**
+   * @var string
+   */
+  protected $address;
+
+  /**
+   * @param string $address
+   *
+   * @retun $this
+   */
+  public function setAddress(string $address) {
+    $this->address = $address;
+
+    return $this;
+  }
 
   /**
    * @var \Drupal\Core\Config\Config|\Drupal\Core\Config\ImmutableConfig
@@ -16,26 +42,87 @@ class GatsbyOrchestratorGatsbyHealth {
   protected $gatsbySettings;
 
   /**
-   * @var Messenger
+   * @var MessengerInterface
    */
   protected $messenger;
 
   /**
-   * @var \GuzzleHttp\Client
+   * @var \GuzzleHttp\ClientInterface
    */
   protected $httpClient;
 
   /**
    * GatsbyRevisionGatsbyHealth constructor.
    *
-   * @param \Drupal\Core\Config\ConfigFactory $config_factory
-   * @param \Drupal\Core\Messenger\Messenger $messenger
-   * @param \GuzzleHttp\Client $http_client
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   * @param \Drupal\Core\Messenger\MessengerInterface $messenger
+   * @param \GuzzleHttp\ClientInterface $http_client
    */
-  public function __construct(\Drupal\Core\Config\ConfigFactory $config_factory, Messenger $messenger, \GuzzleHttp\Client $http_client) {
+  public function __construct(\Drupal\Core\Config\ConfigFactoryInterface $config_factory, MessengerInterface $messenger, \GuzzleHttp\ClientInterface $http_client) {
     $this->gatsbySettings = $config_factory->get('gatsby.settings');
     $this->messenger = $messenger;
     $this->httpClient = $http_client;
+  }
+
+  /**
+   * @return \Drupal\Core\Config\Config|\Drupal\Core\Config\ImmutableConfig
+   */
+  public function getGatsbySettings() {
+    return $this->gatsbySettings;
+  }
+
+  /**
+   * @param \Drupal\Core\Config\Config|\Drupal\Core\Config\ImmutableConfig $gatsbySettings
+   */
+  public function setGatsbySettings($gatsbySettings) {
+    $this->gatsbySettings = $gatsbySettings;
+
+    return $this;
+  }
+
+  /**
+   * @return MessengerInterface
+   */
+  public function getMessenger(): MessengerInterface {
+    return $this->messenger;
+  }
+
+  /**
+   * @param MessengerInterface $messenger
+   */
+  public function setMessenger(MessengerInterface $messenger) {
+    $this->messenger = $messenger;
+
+    return $this;
+  }
+
+  /**
+   * @return \GuzzleHttp\ClientInterface
+   */
+  public function getHttpClient(): \GuzzleHttp\ClientInterface {
+    return $this->httpClient;
+  }
+
+  /**
+   * @param \GuzzleHttp\ClientInterface $httpClient
+   */
+  public function setHttpClient(\GuzzleHttp\ClientInterface $httpClient) {
+    $this->httpClient = $httpClient;
+
+    return $this;
+  }
+
+  /**
+   * @return array|mixed|string|null
+   */
+  public function getAddress() {
+
+    if (!$this->address) {
+      $this->address = $this->gatsbySettings->get('server_url');
+      return $this->address;
+    }
+
+    return $this->address;
   }
 
   /**
@@ -45,7 +132,7 @@ class GatsbyOrchestratorGatsbyHealth {
    *  True in case the server is up false if not.
    */
   public function checkGatsbyHealth() {
-    $address = $this->gatsbySettings->get('server_url');
+    $address = $this->getAddress();
 
     if (!$address) {
       $this->messenger->addError(t('It seems that the address of the gatsby server has not been set.'));
---
+++ tests/src/Kernel/GatsbyOrchestratorGatsbyHealthTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Drupal\Tests\gatsby_orchestrator\Kernel;
+
+use Drupal\gatsby_orchestrator\GatsbyOrchestratorGatsbyHealth;
+use Drupal\KernelTests\KernelTestBase;
+use Drupal\Tests\gatsby_orchestrator\Kernel\Mocks\MessengerMock;
+use GuzzleHttp\Handler\MockHandler;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Exception\RequestException;
+
+/**
+ * Test description.
+ *
+ * @group gatsby_orchestrator
+ */
+class GatsbyOrchestratorGatsbyHealthTest extends KernelTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = ['gatsby_orchestrator'];
+
+  /**
+   * @var GatsbyOrchestratorGatsbyHealth
+   */
+  protected $gatsbyHealth;
+
+  /**
+   * @var MessengerMock
+   */
+  protected $messenger;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    $mock = new MockHandler([
+      new RequestException('Error Communicating with Server', new Request('GET', 'test')),
+      new Response(200, ['X-Foo' => 'Bar'], 'Hello, World'),
+    ]);
+
+    $handlerStack = HandlerStack::create($mock);
+    $client = new Client(['handler' => $handlerStack]);
+    $this->messenger = new MessengerMock();
+
+    $this->gatsbyHealth = $this->container->get('gatsby_orchestrator.gatsby_health');
+    $this->gatsbyHealth
+      ->setHttpClient($client)
+      ->setMessenger($this->messenger);
+  }
+
+  /**
+   * Test callback.
+   */
+  public function testSomething() {
+    // Send a request without an address and make sure we an error is tracked.
+    $this->gatsbyHealth->checkGatsbyHealth();
+
+    $this->assertEquals(
+      "It seems that the address of the gatsby server has not been set.",
+      $this->messenger->errors[0]
+    );
+
+    // Set a dummy address and see how we handle mocked results.
+    $results = $this
+      ->gatsbyHealth
+      ->setAddress('http://google.com')
+      ->checkGatsbyHealth();
+
+    $this->assertEquals(GatsbyOrchestratorGatsbyHealth::GATSBY_SERVICE_DOWN, $results);
+    $this->assertEquals( "Error Communicating with Server", $this->messenger->errors[1]);
+
+    // Wait for a successful request.
+    $results = $this->gatsbyHealth->checkGatsbyHealth();
+    $this->assertEquals(GatsbyOrchestratorGatsbyHealth::GATSBY_SERVICE_UP, $results);
+  }
+
+}
